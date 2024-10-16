7de3bb39f46af472b32092189f9d5b5c93d9dd7d
PuffyDucks <PuffyDucks@users.noreply.github.com>
2024-10-16 09:44:43 -0700

    init

---
+++ block_data.py
@@ -0,0 +1,34 @@
+block_data = {
+    "colour_picker": {
+        "COLOUR": "field"
+    },
+    "math_number": {
+        "NUM": "field"
+    },
+    "draw_fill": {
+        "TYPE": "field",
+        "VALUE_1": "value:colour_picker"
+    },
+    "draw_stroke_color": {
+        "color": "value:colour_picker"
+    },
+    "draw_line_style": {
+        "style": "field"
+    },
+    "array_split_with_type": {
+        "TYPE": "field",
+        "VAR": "field",
+        "TEXT": "field"
+    },
+    "draw_polygon": {
+        "VARX": "field",
+        "VARY": "field"
+    },
+    "draw_line": {
+        "TYPE": "field",
+        "VALUE_1": "value:math_number",
+        "VALUE_2": "value:math_number",
+        "VALUE_3": "value:math_number",
+        "VALUE_4": "value:math_number"
+    }
+}
